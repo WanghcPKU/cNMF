958ec5779a3bd0ae765e76e5cbd0a5e8b2434ed3
WanghcPKU <165775424+WanghcPKU@users.noreply.github.com>
2024-07-24 14:37:09 +0800

    Create step1.sh

---
+++ step1.sh
@@ -0,0 +1,4 @@
+cnmf prepare --output-dir ./ --name isop8.fib_cNMF -c ./matrix.mtx --max-nmf-iter 2000 -k 3 4 5 6 7 8 9 10 --n-iter 20
+
+### --max-nmf-iter 2000 设置最大迭代次数 
+### --n-iter 20 每个k值的迭代次数
